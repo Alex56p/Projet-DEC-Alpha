d70b36cb5bc02f042fbe667086d94edccc7e5770
PoneyLoverXz <antho.voyez@hotmail.com>
2014-12-16 01:27:07 -0500

    Début des classes

---
+++ Projet-DEC-Alpha/Projet-DEC-Alpha/Attaques.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_DEC_Alpha
+{
+    class Attaque
+    {
+        private string Nom;
+        private int Power{get;set;}
+
+        private int Accuracy { get; set; }
+        private TypePokemon Type { get; set; }
+        private int PowerPoint { get; set; }
+
+    }
+}
---
+++ Projet-DEC-Alpha/Projet-DEC-Alpha/Pokemon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_DEC_Alpha
+{
+    enum TypePokemon
+    {
+        Eau, Feu, Herbe
+    };
+
+    class Pokemon
+    {
+        private string Nom{get;set;}
+        private int Level { get; set;}
+        private int Exp { get; set; }
+        private int HP { get; set; }
+
+        private int Attack { get; set; }
+        private int Defense { get; set; }
+        private int Mana { get; set; }
+        private TypePokemon Type { get; set; }
+        
+
+
+    }
+}
